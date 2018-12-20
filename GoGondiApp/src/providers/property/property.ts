b4107d681f8f9126c27e3a689075c249263b6e38
Schmitz <Schmitz>
2018-12-20 13:42:54 +0100

    change color

--- GoGondiApp/src/providers/property/property.ts
+++ GoGondiApp/src/providers/property/property.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable }from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
+import { Property } from '../../models/property';
 
 
 @Injectable()
